897a5ece0e66f734bbd673e1e1d018fe7d33c3ae
siddhant  khokle <120040336+SiddhuKhokle96@users.noreply.github.com>
2025-04-21 17:07:15 +0530

    Add files via upload

---
+++ config/swagger.js
@@ -0,0 +1,10 @@
+module.exports = {
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'Rajashree Foundation API',
+        version: '1.0.0',
+      },
+    },
+    apis: ['./routes/*.js'], // Path to your route files
+  };
